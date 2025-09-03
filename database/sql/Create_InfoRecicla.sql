11434b6854cfe64a11c152c1c188726151895e5d
Andres11Salas <andresmadid1206@gmail.com>
2025-09-02 21:44:55 -0500

    Mejora en la estructura de la base de datos: se agregan y ajustan columnas en las tablas de usuarios, perfiles y otros, para optimizar la gestión de datos.

--- database/sql/Create_InfoRecicla.sql
+++ database/sql/Create_InfoRecicla.sql
@@ -1,37 +1,38 @@
-
-
+--jorge sp 
 -- 1. Crear base de datos y usarla
 CREATE DATABASE IF NOT EXISTS InfoRecicla;
 USE InfoRecicla;
-
 -- 2. Tabla usuarios
 CREATE TABLE usuarios (
   id INT PRIMARY KEY,
-  numero_de_documento int(20) NOT NULL UNIQUE,  
-  tipo_documento ENUM('CedulaDeCiudadania','CedulaDeExtranjeria','Pasaporte','TarjetaDeIdentidad') NOT NULL,
-  avatar_url VARCHAR(255), 
-  telefono VARCHAR(20), 
-  recibe_notificaciones BOOLEAN NOT NULL DEFAULT TRUE,  
-  nombre_usuario VARCHAR(50) NOT NULL UNIQUE, 
+  numero_de_documento int(20) NOT NULL UNIQUE,
+  tipo_documento ENUM(
+    'CedulaDeCiudadania',
+    'CedulaDeExtranjeria',
+    'Pasaporte',
+    'TarjetaDeIdentidad'
+  ) NOT NULL,
+  avatar_url VARCHAR(255),
+  telefono VARCHAR(20),
+  recibe_notificaciones BOOLEAN NOT NULL DEFAULT TRUE,
+  nombre_usuario VARCHAR(50) NOT NULL UNIQUE,
   correo VARCHAR(100) NOT NULL,
   contraseña VARCHAR(255) NOT NULL,
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
   direccion VARCHAR(150),
-  fecha_nacimiento DATE, 
-  localidad VARCHAR(50),  
-  genero ENUM('Masculino','Femenino','Otro'),
-  rol ENUM('Ciudadano','GestorECA','Administrador') NOT NULL,
+  fecha_nacimiento DATE,
+  localidad VARCHAR(50),
+  genero ENUM('Masculino', 'Femenino', 'Otro'),
+  rol ENUM('Ciudadano', 'GestorECA', 'Administrador') NOT NULL,
   creado DATETIME NOT NULL
 );
-
 -- 3. Tabla perfiles_ciudadano
 CREATE TABLE perfiles_ciudadano (
   usuario_id INT PRIMARY KEY,
   edad INT,
   ubicacion VARCHAR(100)
 );
-
 -- 4. Tabla perfiles_punto_eca
 CREATE TABLE perfiles_punto_eca (
   usuario_id INT PRIMARY KEY,
@@ -39,83 +40,74 @@ CREATE TABLE perfiles_punto_eca (
   imagen_perfil_url VARCHAR(255),
   telefono VARCHAR(20)
 );
-
 -- 5. Tabla ciudades
 CREATE TABLE ciudades (
   id INT PRIMARY KEY,
   nombre VARCHAR(100) NOT NULL
 );
-
-
 -- 6. Tabla localidades
 CREATE TABLE localidades (
   id INT PRIMARY KEY,
   ciudad_id INT NOT NULL,
   nombre VARCHAR(100) NOT NULL
 );
-
 -- 7. Tabla puntos_eca
 CREATE TABLE puntos_eca (
   id INT PRIMARY KEY,
   gestor_id INT NOT NULL,
-  nit VARCHAR(20) NOT NULL UNIQUE,  
+  nit VARCHAR(20) NOT NULL UNIQUE,
   nombre VARCHAR(100) NOT NULL,
   descripcion TEXT,
   direccion VARCHAR(150) NOT NULL,
-  horario_atencion VARCHAR(100), 
-  sitio_web VARCHAR(100), 
-  logo_url VARCHAR(255), 
-  foto_url VARCHAR(255),  
-  mostrar_mapa BOOLEAN NOT NULL DEFAULT TRUE, 
+  horario_atencion VARCHAR(100),
+  sitio_web VARCHAR(100),
+  logo_url VARCHAR(255),
+  foto_url VARCHAR(255),
+  mostrar_mapa BOOLEAN NOT NULL DEFAULT TRUE,
   ciudad_id INT NOT NULL,
   localidad_id INT,
-  latitud DECIMAL(9,6),
-  longitud DECIMAL(9,6),
+  latitud DECIMAL(9, 6),
+  longitud DECIMAL(9, 6),
   correo VARCHAR(100),
   telefono VARCHAR(20)
 );
-
 -- 8. Tabla materiales
 CREATE TABLE materiales (
   id INT PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
   descripcion TEXT,
-  tipo VARCHAR(20), 
+  tipo VARCHAR(20),
   categoria VARCHAR(20),
   imagen_url VARCHAR(255),
-  precio_compra DECIMAL(7,2),
-  precio_venta DECIMAL(7,2)
+  precio_compra DECIMAL(7, 2),
+  precio_venta DECIMAL(7, 2)
 );
-
 -- 9. Tabla capacidad_material_punto_eca
 CREATE TABLE capacidad_material_punto_eca (
   id INT PRIMARY KEY,
   punto_eca_id INT NOT NULL,
   tipo_id INT NOT NULL,
   material_id INT NOT NULL,
-  capacidad_maxima_kg DECIMAL(10,2) NOT NULL,
-  umbral_alerta DECIMAL(5,2) NOT NULL
+  capacidad_maxima_kg DECIMAL(10, 2) NOT NULL,
+  umbral_alerta DECIMAL(5, 2) NOT NULL
 );
-
 -- 10. Tabla inventario
 CREATE TABLE inventario (
   id INT PRIMARY KEY,
   punto_eca_id INT NOT NULL,
   material_id INT NOT NULL,
-  cantidad DECIMAL(10,2) NOT NULL,
+  cantidad DECIMAL(10, 2) NOT NULL,
   fecha_registro TIMESTAMP NOT NULL DEFAULT NOW(),
-  stock_actual decimal (10,2), 
-  umbral_alerta decimal (10,2), 
-  umbral_critico decimal (10,2) 
+  stock_actual decimal (10, 2),
+  umbral_alerta decimal (10, 2),
+  umbral_critico decimal (10, 2)
 );
-
 -- 11. Tabla tipos_publicacion
 CREATE TABLE tipos_publicacion (
   id INT PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
   descripcion TEXT
 );
-
 -- 12. Tabla publicaciones
 CREATE TABLE publicaciones (
   id INT PRIMARY KEY,
@@ -135,7 +127,7 @@ CREATE TABLE publicaciones (
 create table categorias_publicaciones(
   id INT PRIMARY KEY,
   publicacion_id INT NOT NULL,
-  tipo ENUM('imagen','video','documento','enlace') NOT NULL,
+  tipo ENUM('imagen', 'video', 'documento', 'enlace') NOT NULL,
   url VARCHAR(255) NOT NULL,
   titulo VARCHAR(50),
   descripcion TEXT
@@ -145,22 +137,21 @@ create table categorias_publicaciones(
 -- Se agrega tabla Etiquetas
 -- 14. Tabla votos
 create table votos(
-  tipo ENUM('pulicacion','punto eca'),
+  tipo ENUM('pulicacion', 'punto eca'),
   referencia_id INT NOT NULL,
   usuario_id INT NOT NULL,
-  valor ENUM('like','dislike'),
+  valor ENUM('like', 'dislike'),
   creado TIMESTAMP NOT NULL DEFAULT NOW()
 );
 -- -----------------------------------
 -- -----------------------------------
 -- 13. Tabla favoritos
 CREATE TABLE favoritos (
-  id INT  PRIMARY KEY,
+  id INT PRIMARY KEY,
   usuario_id INT NOT NULL,
   publicacion_id INT NOT NULL,
   creado TIMESTAMP NOT NULL DEFAULT NOW()
 );
-
 -- 14. Tabla comentarios
 CREATE TABLE comentarios (
   id INT PRIMARY KEY,
@@ -170,35 +161,31 @@ CREATE TABLE comentarios (
   contenido TEXT NOT NULL,
   texto TEXT,
   creado TIMESTAMP NOT NULL DEFAULT NOW(),
-  tipo ENUM('publicacion','punto eca'),
+  tipo ENUM('publicacion', 'punto eca'),
   referencia_id INT NOT NULL
 );
-
 -- 15. Tabla notificaciones
 CREATE TABLE notificaciones (
-  id INT  PRIMARY KEY,
+  id INT PRIMARY KEY,
   usuario_id INT NOT NULL,
   tipo_id INT NOT NULL,
   referencia_id INT NOT NULL,
   enviado TIMESTAMP NOT NULL DEFAULT NOW(),
   leido TIMESTAMP NULL
 );
-
 -- 16. Tabla tipos_notificacion
 CREATE TABLE tipos_notificacion (
   id INT PRIMARY KEY,
   codigo VARCHAR(50) NOT NULL UNIQUE,
   descripcion TEXT
 );
-
 -- 17. Tabla conversaciones
 CREATE TABLE conversaciones (
-  id INT  PRIMARY KEY,
+  id INT PRIMARY KEY,
   usuario_eca_id INT NOT NULL,
   usuario_ciudadano_id INT NOT NULL,
   iniciado DATETIME NOT NULL
 );
-
 -- 18. Tabla mensajes
 CREATE TABLE mensajes (
   id INT PRIMARY KEY,
@@ -208,23 +195,29 @@ CREATE TABLE mensajes (
   enviado DATETIME NOT NULL,
   leido DATETIME
 );
-
 -- 19. Tabla tipos_material
 CREATE TABLE tipos_material (
   id INT PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL UNIQUE,
-  descripcion TEXT);
-
+  descripcion TEXT
+);
 -- 20. Tabla programacion_recoleccion
 CREATE TABLE programacion_recoleccion (
   id INT PRIMARY KEY,
   punto_eca_id INT NOT NULL,
   material_id INT NOT NULL,
-  planta_reciclaje_id INT,  
-  dia_semana ENUM('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo') NOT NULL,
+  planta_reciclaje_id INT,
+  dia_semana ENUM(
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+    'Domingo'
+  ) NOT NULL,
   frecuencia_semanas INT NOT NULL
 );
-
 -- 21. Tabla plantas_reciclaje 
 CREATE TABLE plantas_reciclaje (
   id INT PRIMARY KEY,
@@ -235,14 +228,12 @@ CREATE TABLE plantas_reciclaje (
   frecuencia_recoleccion VARCHAR(100),
   direccion VARCHAR(255)
 );
-
 -- 22. Tabla materiales_plantas_reciclaje 
 CREATE TABLE materiales_plantas_reciclaje (
   planta_id INT NOT NULL,
   material_id INT NOT NULL,
   PRIMARY KEY (planta_id, material_id)
 );
-
 -- -------------------------------------
 -- Se agrega tabla Compras
 -- 23. Tabla compras
@@ -250,8 +241,8 @@ create table compras(
   id INT PRIMARY KEY,
   inventario_id INT NOT NULL,
   fecha TIMESTAMP NOT NULL DEFAULT NOW(),
-  kg decimal (6,2),
-  precio_unitario decimal (7,2)
+  kg decimal (6, 2),
+  precio_unitario decimal (7, 2)
 );
 -- -------------------------------------
 -- ------------------------------------
@@ -262,7 +253,7 @@ create table salidas(
   inventario_id INT NOT NULL,
   centros_de_acopio_id INT NOT NULL,
   fecha TIMESTAMP NOT NULL DEFAULT NOW(),
-  kg decimal (6,2)
+  kg decimal (6, 2)
 );
 -- ------------------------------------
 -- -----------------------------------
@@ -274,7 +265,13 @@ create table despachos(
   centros_de_acopio_id INT NOT NULL,
   fecha date NOT NULL DEFAULT NOW(),
   hora time NOT NULL DEFAULT NOW(),
-  frecuencia enum('manual', 'semanal', 'quincenal', 'mensual', 'unico') default 'manual'
+  frecuencia enum(
+    'manual',
+    'semanal',
+    'quincenal',
+    'mensual',
+    'unico'
+  ) default 'manual'
 );
 -- -----------------------------------
 -- -------------------------------------------------------
@@ -284,7 +281,7 @@ create table centros_de_acopio(
   id INT PRIMARY KEY,
   nombre varchar(150) NOT NULL,
   nit varchar(20) UNIQUE,
-  alcance ENUM('Global','ECA'),
+  alcance ENUM('Global', 'ECA'),
   id_eca INT,
   contacto varchar(20),
   telefono varchar(20),
@@ -292,91 +289,71 @@ create table centros_de_acopio(
   direccion varchar(255),
   localidad_id INT not null,
   ciudad_id INT not null,
-  latitud DECIMAL(9,6),
-  longitud DECIMAL(9,6),  
-  estado ENUM('Activo','Inactivo') NOT NULL DEFAULT 'Activo',
+  latitud DECIMAL(9, 6),
+  longitud DECIMAL(9, 6),
+  estado ENUM('Activo', 'Inactivo') NOT NULL DEFAULT 'Activo',
   creado TIMESTAMP NOT NULL DEFAULT NOW(),
   actualizado TIMESTAMP NOT NULL DEFAULT NOW()
 );
 -- ----------------------------------------------------------------
 -- 23. Claves foráneas al final
 ALTER TABLE perfiles_ciudadano
-  ADD CONSTRAINT fk_perfiles_ciudadano_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id);
-
+ADD CONSTRAINT fk_perfiles_ciudadano_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id);
 ALTER TABLE perfiles_punto_eca
-  ADD CONSTRAINT fk_perfiles_punto_eca_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+ADD CONSTRAINT fk_perfiles_punto_eca_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
   ADD CONSTRAINT fk_perfiles_punto_eca_punto_eca_id FOREIGN KEY (punto_eca_id) REFERENCES puntos_eca(id);
-
 ALTER TABLE localidades
-  ADD CONSTRAINT fk_localidades_ciudad_id FOREIGN KEY (ciudad_id) REFERENCES ciudades(id);
-
+ADD CONSTRAINT fk_localidades_ciudad_id FOREIGN KEY (ciudad_id) REFERENCES ciudades(id);
 ALTER TABLE puntos_eca
-  ADD CONSTRAINT fk_puntos_eca_gestor_id FOREIGN KEY (gestor_id) REFERENCES usuarios(id),
+ADD CONSTRAINT fk_puntos_eca_gestor_id FOREIGN KEY (gestor_id) REFERENCES usuarios(id),
   ADD CONSTRAINT fk_puntos_eca_ciudad_id FOREIGN KEY (ciudad_id) REFERENCES ciudades(id),
   ADD CONSTRAINT fk_puntos_eca_localidad_id FOREIGN KEY (localidad_id) REFERENCES localidades(id);
-
 ALTER TABLE capacidad_material_punto_eca
-  ADD CONSTRAINT fk_capacidad_material_punto_eca_punto_eca_id FOREIGN KEY (punto_eca_id) REFERENCES puntos_eca(id),
+ADD CONSTRAINT fk_capacidad_material_punto_eca_punto_eca_id FOREIGN KEY (punto_eca_id) REFERENCES puntos_eca(id),
   ADD CONSTRAINT fk_capacidad_material_punto_eca_material_id FOREIGN KEY (material_id) REFERENCES materiales(id),
-    ADD CONSTRAINT fk_capacidad_material_punto_eca_tipo_id FOREIGN KEY (tipo_id) REFERENCES tipos_material(id);
-
+  ADD CONSTRAINT fk_capacidad_material_punto_eca_tipo_id FOREIGN KEY (tipo_id) REFERENCES tipos_material(id);
 ALTER TABLE inventario
-  ADD CONSTRAINT fk_inventario_punto_eca_id FOREIGN KEY (punto_eca_id) REFERENCES puntos_eca(id),
+ADD CONSTRAINT fk_inventario_punto_eca_id FOREIGN KEY (punto_eca_id) REFERENCES puntos_eca(id),
   ADD CONSTRAINT fk_inventario_material_id FOREIGN KEY (material_id) REFERENCES materiales(id);
-
 ALTER TABLE publicaciones
-  ADD CONSTRAINT fk_publicaciones_autor_id FOREIGN KEY (autor_id) REFERENCES usuarios(id),
+ADD CONSTRAINT fk_publicaciones_autor_id FOREIGN KEY (autor_id) REFERENCES usuarios(id),
   ADD CONSTRAINT fk_publicaciones_tipo_id FOREIGN KEY (tipo_id) REFERENCES tipos_publicacion(id);
-
 ALTER TABLE favoritos
-  ADD CONSTRAINT fk_favoritos_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+ADD CONSTRAINT fk_favoritos_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
   ADD CONSTRAINT fk_favoritos_publicacion_id FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id);
-
 ALTER TABLE comentarios
-  ADD CONSTRAINT fk_comentarios_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+ADD CONSTRAINT fk_comentarios_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
   ADD CONSTRAINT fk_comentarios_publicacion_id FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id),
   ADD CONSTRAINT fk_comentarios_punto_eca_id FOREIGN KEY (punto_eca_id) REFERENCES puntos_eca(id);
-
 ALTER TABLE notificaciones
-  ADD CONSTRAINT fk_notificaciones_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+ADD CONSTRAINT fk_notificaciones_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
   ADD CONSTRAINT fk_notificaciones_tipo_id FOREIGN KEY (tipo_id) REFERENCES tipos_notificacion(id);
-
 ALTER TABLE conversaciones
-  ADD CONSTRAINT fk_conversaciones_usuario_eca_id FOREIGN KEY (usuario_eca_id) REFERENCES usuarios(id),
+ADD CONSTRAINT fk_conversaciones_usuario_eca_id FOREIGN KEY (usuario_eca_id) REFERENCES usuarios(id),
   ADD CONSTRAINT fk_conversaciones_usuario_ciudadano_id FOREIGN KEY (usuario_ciudadano_id) REFERENCES usuarios(id);
-
 ALTER TABLE mensajes
-  ADD CONSTRAINT fk_mensajes_conversacion_id FOREIGN KEY (conversacion_id) REFERENCES conversaciones(id),
+ADD CONSTRAINT fk_mensajes_conversacion_id FOREIGN KEY (conversacion_id) REFERENCES conversaciones(id),
   ADD CONSTRAINT fk_mensajes_remitente_id FOREIGN KEY (remitente_id) REFERENCES usuarios(id);
-
 ALTER TABLE programacion_recoleccion
-  ADD CONSTRAINT fk_programacion_recoleccion_punto_eca_id FOREIGN KEY (punto_eca_id) REFERENCES puntos_eca(id),
+ADD CONSTRAINT fk_programacion_recoleccion_punto_eca_id FOREIGN KEY (punto_eca_id) REFERENCES puntos_eca(id),
   ADD CONSTRAINT fk_programacion_recoleccion_material_id FOREIGN KEY (material_id) REFERENCES materiales(id),
   ADD CONSTRAINT fk_programacion_recoleccion_plantas_reciclaje_id FOREIGN KEY (planta_reciclaje_id) REFERENCES plantas_reciclaje(id);
-
 ALTER TABLE materiales_plantas_reciclaje
-  ADD CONSTRAINT fk_materiales_plantas_reciclaje_planta_id FOREIGN KEY (planta_id) REFERENCES plantas_reciclaje(id),
+ADD CONSTRAINT fk_materiales_plantas_reciclaje_planta_id FOREIGN KEY (planta_id) REFERENCES plantas_reciclaje(id),
   ADD CONSTRAINT fk_materiales_plantas_reciclaje_material_id FOREIGN KEY (material_id) REFERENCES materiales(id);
-
 ALTER TABLE categorias_publicaciones
-  ADD CONSTRAINT fk_categorias_publicaciones_publicacion_id FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id);
-
-ALTER TABLE votos 
-  ADD CONSTRAINT fk_votos_usurios_id FOREIGN KEY (usuarios_id) REFERENCES usuarios(id);
-
+ADD CONSTRAINT fk_categorias_publicaciones_publicacion_id FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id);
+ALTER TABLE votos
+ADD CONSTRAINT fk_votos_usurios_id FOREIGN KEY (usuarios_id) REFERENCES usuarios(id);
 ALTER TABLE comentarios
-  ADD CONSTRAINT fk_comentarios_usurios_id FOREIGN KEY (usuarios_id) REFERENCES usuarios(id);
-
+ADD CONSTRAINT fk_comentarios_usurios_id FOREIGN KEY (usuarios_id) REFERENCES usuarios(id);
 ALTER TABLE compras
-  ADD CONSTRAINT fk_comentarios_inventario_id FOREIGN KEY (inventario_id) REFERENCES inventario(id);
-
+ADD CONSTRAINT fk_comentarios_inventario_id FOREIGN KEY (inventario_id) REFERENCES inventario(id);
 ALTER TABLE entradas
-  ADD CONSTRAINT fk_entradas_inventario_id FOREIGN KEY (inventario_id) REFERENCES inventario(id);
-
+ADD CONSTRAINT fk_entradas_inventario_id FOREIGN KEY (inventario_id) REFERENCES inventario(id);
 ALTER TABLE salidas
-  ADD CONSTRAINT fk_salidas_inventario_id FOREIGN KEY (inventario_id) REFERENCES inventario(id);
-  ADD CONSTRAINT fk_salidas_centro_de_acopio_id FOREIGN KEY (centros_de_acopio_id) REFERENCES centros_de_acopio(id);
-
+ADD CONSTRAINT fk_salidas_inventario_id FOREIGN KEY (inventario_id) REFERENCES inventario(id);
+ADD CONSTRAINT fk_salidas_centro_de_acopio_id FOREIGN KEY (centros_de_acopio_id) REFERENCES centros_de_acopio(id);
 ALTER TABLE despachos
-  ADD CONSTRAINT fk_despacho_inventario_id FOREIGN KEY (inventario_id) REFERENCES inventario(id);
-  ADD CONSTRAINT fk_despacho_centro_de_acopio_id FOREIGN KEY (centros_de_acopio_id) REFERENCES centros_de_acopio(id);
+ADD CONSTRAINT fk_despacho_inventario_id FOREIGN KEY (inventario_id) REFERENCES inventario(id);
+ADD CONSTRAINT fk_despacho_centro_de_acopio_id FOREIGN KEY (centros_de_acopio_id) REFERENCES centros_de_acopio(id);
