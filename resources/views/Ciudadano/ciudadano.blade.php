611555ed74f6a76d35b9646289297aafa1ee7969
jlbf2005 <programacionjorgeb@gmail.com>
2025-09-19 06:36:27 -0500

    comiit de blade de ciudadano

--- resources/views/Ciudadano/ciudadano.blade.php
+++ resources/views/Ciudadano/ciudadano.blade.php
@@ -41,6 +41,8 @@
 
         <div class="tab-content pt-3">
             <!-- ===== GUARDADOS ===== -->
+
+
             <section class="tab-pane fade" id="guardados">
                 <div class="row g-4">
                     <div class="col-md-6">
@@ -142,7 +144,7 @@
                 </div>
             </section>
 
-            <!-- ===== AJUSTES (FORM REAL) ===== -->
+
             <section class="tab-pane fade" id="ajustes">
                 <div class="card card-hover">
                     <div class="card-body">
@@ -159,47 +161,72 @@
                                     respuestas y novedades</label>
                             </div>
 
+                        <div class="form-check form-switch mb-3">
+                            <input class="form-check-input" type="checkbox" role="switch" id="prefNoti">
+                            <label class="form-check-label" for="prefNoti">Recibir notificaciones de publicaciones,
+                                respuestas y novedades</label>
+                        </div>
 
-                            <div class="mb-3">
-                                <label class="form-label" for="nombre_usuario">Nombre de Usuario</label>
-                                <input type="text" class="form-control" id="nombre_usuario" name="nombre_usuario"
-                                    value="{{ $usuario->nombre_usuario }}" required>
-                            </div>
 
-                            <div class="mb-3">
-                                <label class="form-label">Nombre</label>
-                                <input type="text" class="form-control" id="editNombre"
-                                    value="{{ $usuario->nombre }}" required>
-                            </div>
-                            <div class="mb-3">
-                                <label class="form-label">Correo</label>
-                                <input type="email" class="form-control" id="editCorreo"
-                                    value="{{ $usuario->correo }}" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="nombre_usuario" id="editNombreUsuario" class="form-label">Nombre de
-                                    usuario</label>
-                                <input type="text" class="form-control" id="editNombreUsuario"
-                                    value="{{ $usuario->nombre_usuario }}" required>
-                            </div>
-                            <div class="mb-3">
-                                <label class="form-label">Localidad</label>
-                                <input type="text" class="form-control" id="editLocalidad" value="">
-                            </div>
-                            <div class="mb-3">
-                                <label class="form-label">Foto de perfil (opcional)</label>
-                                <input type="file" class="form-control" id="editAvatar" accept="image/*">
-                                <div class="form-text">Formatos aceptados: JPG, PNG, WEBP. Máx. 2 MB.</div>
-                            </div>
-                            <div>
-                                <label for="old_password" class="form-label">Contraseña actual</label><input
-                                    type="password" class="form-control" id="old_password"></label>
-                                <label for="password" class="form-label">Contraseña nueva</label><input
-                                    type="password" class="form-control" id="password"></label>
-                                <label for="password_confirmation" class="form-label">Confirmar
-                                    contraseña</label><input type="password" class="form-control"
-                                    id="password_confirmation">
-                            </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="displayName">Nombre de Usuario</label>
+                            <input type="text" class="form-control" id="displayName"
+                                placeholder="Como aparecerás al comentar" value="{{ $usuario->nombre_usuario }}">
+                        </div>
+
+
+                        <div class="text-end">
+                            <button class="btn btn-success" id="btnGuardarAjustes">Guardar preferencias</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="editarPerfilModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <form class="modal-content" id="editarPerfilForm">
+                <div class="modal-header bg-success text-white">
+                    <h5 class="modal-title">Editar perfil</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
+                        aria-label="Cerrar"></button>
+                </div>
+                <div class="modal-body">
+
+                    <div class="mb-3">
+                        <label class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="editNombre" value="{{ $usuario->nombre }}" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Correo</label>
+                        <input type="email" class="form-control" id="editCorreo" value="{{ $usuario->correo }}"
+                            required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="nombre_usuario" id="editNombreUsuario" class="form-label">Nombre de
+                            usuario</label>
+                        <input type="text" class="form-control" id="editNombreUsuario" value="{{ $usuario->nombre_usuario }}"
+                            required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Localidad</label>
+                        <input type="text" class="form-control" id="editLocalidad" value="">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Foto de perfil (opcional)</label>
+                        <input type="file" class="form-control" id="editAvatar" accept="image/*">
+                        <div class="form-text">Formatos aceptados: JPG, PNG, WEBP. Máx. 2 MB.</div>
+                    </div>
+                    <div>
+                        <label for="old_password" class="form-label">Contraseña actual</label><input type="password"
+                            class="form-control" id="old_password"></label>
+                        <label for="password" class="form-label">Contraseña nueva</label><input type="password"
+                            class="form-control" id="password"></label>
+                        <label for="password_confirmation" class="form-label">Confirmar contraseña</label><input
+                            type="password" class="form-control" id="password_confirmation">
                     </div>
             </section>
         </div>
@@ -215,6 +242,7 @@
             </div>
         </div>
     </div>
-    {{-- comentario --}}
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </x-app-layout>
